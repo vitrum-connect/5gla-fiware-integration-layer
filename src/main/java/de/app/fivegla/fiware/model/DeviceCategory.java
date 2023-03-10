2b011b2198823a2d7de3f366aadeb25223f0d46f
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-03-10 22:43:00 +0100

    Create device. (#4)
    Add integration test.
    Add models.
    Exclude integration tests.
    Prepare releases / Update version.

---
+++ src/main/java/de/app/fivegla/fiware/model/DeviceCategory.java
@@ -0,0 +1,19 @@
+package de.app.fivegla.fiware.model;
+
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+/**
+ * Device category model.
+ * <p>
+ * See <a href="https://github.com/smart-data-models/dataModel.Device/blob/master/Device/doc/spec.md">the smart data models</a> for more information.
+ * <p>
+ */
+@Getter
+@Builder
+public class DeviceCategory {
+    private final String type = "Text";
+    private List<String> value;
+}
