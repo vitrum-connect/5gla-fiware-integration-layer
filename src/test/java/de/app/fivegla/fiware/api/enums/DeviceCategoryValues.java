a198b1a551474d14b34b05b281e6b4d24447118b
Sascha Doemer <Cf4ThQgxcnxAovOUXTNv@saschadoemer.de>
2023-08-12 21:47:19 +0200

    Remove code smell.

--- src/test/java/de/app/fivegla/fiware/api/enums/DeviceCategoryValues.java
+++ src/test/java/de/app/fivegla/fiware/api/enums/DeviceCategoryValues.java
@@ -5,13 +5,13 @@ import lombok.Getter;
 /**
  * Device category values.
  */
+@Getter
 public enum DeviceCategoryValues {
 
     SoilScoutSensor("soilScoutSensor"),
     Farm21Sensor("farm21Sensor"),
     MicaSenseDrone("micaSenseDrone");
 
-    @Getter
     private final String key;
 
     DeviceCategoryValues(String key) {
